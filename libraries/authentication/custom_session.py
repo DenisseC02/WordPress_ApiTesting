7f43b68ab96bd35af95528c7070b48590ff9a980
Roger Renjifo <rrrenjifo@gmail.com>
2023-04-17 17:01:10 -0400

    Delete copyright from scripts

--- libraries/authentication/custom_session.py
+++ libraries/authentication/custom_session.py
@@ -1,17 +1,3 @@
- #
- #  @custom_session.py Copyright (c) 2023 Jalasoft.                            #
- #  2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.        #
- #                                                                             #
- #  All rights reserved.                                                       #
- #                                                                             #
- #  This software is the confidential and proprietary information of           #
- #  Jalasoft, ('Confidential Information'). You shall not                      #
- #  disclose such Confidential Information and shall use it only in            #
- #  accordance with the terms of the license agreement you entered into        #
- #  with Jalasoft.                                                             #
- #
-
-
 from os import getenv
 from libraries.authentication.api_key import Api_Key
 from libraries.authentication.basic import Basic
