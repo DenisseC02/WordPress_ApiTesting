dc22095e004ff24b8fc8c775fffebb842fa781c8
telma <te.nem021@gmail.com>
2023-04-16 21:59:43 -0400

    config white lines

--- libraries/authentication/api_key.py
+++ libraries/authentication/api_key.py
@@ -18,7 +18,7 @@ from libraries.authentication.authorization import Authorization
 
 class Api_Key(Authorization):
     """Generates users valid API key"""
-    
+
     def authentication(self, key, token, add_to) -> tuple:
         """Returns session authorization using API key method""" 
         params = {'key': key, 'token': token}
--- libraries/authentication/authorization.py
+++ libraries/authentication/authorization.py
@@ -18,7 +18,7 @@ from abc import abstractmethod
 
 class Authorization(ABC):
     """Parent authorization class"""
-    
+
     @abstractmethod
     def authentication(self, *kwargs) -> tuple:
         """Returns session authorization"""
