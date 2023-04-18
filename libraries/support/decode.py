5109609c22691aa047bcf4d4d082df3972a657b1
daniskuvc <117122565+daniskuvc@users.noreply.github.com>
2023-04-17 23:05:19 -0400

    Delete decode.py

--- libraries/support/decode.py
+++
@@ -1,6 +0,0 @@
-class Decode(object):
-    @staticmethod
-    def decode_response(response):
-        decoded_result = response.content
-        decoded_result.decode('utf-8')
-        return decoded_result
