f872de79e8bfcf2768e4c9edd93b201a6009fc52
Rocío Morales <99517293+Rochius@users.noreply.github.com>
2023-04-18 00:34:02 -0300

    Update oauth2.py

--- libraries/authentication/oauth2.py
+++ libraries/authentication/oauth2.py
@@ -8,14 +8,11 @@ class OAuth2(Authorization):
     def authentication(self, server, client_id, client_secret, **kwargs):
         """Returns a session using OAuth 2.0"""
         params = {}
-
         headers = {'content-type': 'application/x-www-form-urlencoded'}
         data = {'grant_type': 'client_credentials', 'client_id': client_id,
                 'client_secret': client_secret}
         response = requests.post(server, headers=headers, data=data)
-
         session = requests.Session()
         session.headers.update(
             {'Authorization': 'Bearer ' + response.json()['access_token']})
-
         return session, params
