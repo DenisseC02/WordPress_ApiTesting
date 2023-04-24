bfab3ffe531450a57184ad1901a13b8efdfd4613
Rocío Morales <99517293+Rochius@users.noreply.github.com>
2023-04-24 13:40:23 -0300

    Update url_assembler.py

--- libraries/process_data/url_assembler.py
+++ libraries/process_data/url_assembler.py
@@ -5,10 +5,10 @@ class UrlAssembler:
     '''Assemble the end point URL'''
 
     def get_url(self, **kwargs):
-        """Returns the URL"""
-        base_url = getenv("HOST")
-        end_point = getenv("END_POINT")
-        port = getenv("PORT")
+        '''Returns the URL'''
+        base_url = getenv('HOST')
+        end_point = getenv('END_POINT')
+        port = getenv('PORT')
         url = '%s:%s/%s' % (base_url, port, end_point)
         for extra_path in kwargs:
             extra_path_value = kwargs.get(extra_path)
