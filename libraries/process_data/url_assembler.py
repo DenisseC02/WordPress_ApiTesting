bac9a51891968501ad7d899f3adcc16ed71ccdaf
Rocio Morales <roncio.93@gmail.com>
2023-04-25 10:52:37 -0300

    Update url_assembler

--- libraries/process_data/url_assembler.py
+++ libraries/process_data/url_assembler.py
@@ -8,7 +8,8 @@ class UrlAssembler:
         '''Returns the URL'''
         base_url = getenv('HOST')
         end_point = getenv('END_POINT')
-        port = getenv('PORT')
+        # port = getenv('PORT')
+        # url = '%s:%s/%s' % (base_url, port, end_point)
         url = '%s/%s' % (base_url, end_point)
         for extra_path in kwargs:
             extra_path_value = kwargs.get(extra_path)
