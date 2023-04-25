8f5be41263953bc651c033024ef1b6625addff7c
telma <te.nem021@gmail.com>
2023-04-25 17:04:45 -0400

    refactor pages

---
+++ libraries/process_data/decode.py
@@ -0,0 +1,8 @@
+class Decode(object):
+    '''Decodes end point response'''
+
+    @staticmethod
+    def decode_response(response):
+        '''Returns decoded response'''
+        decoded_result = response.content.decode('utf-8')
+        return decoded_result
